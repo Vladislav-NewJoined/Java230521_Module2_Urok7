38cd9f8e2717c172e915a295f70fa73330dfc86f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-21 20:20:25 +0300

    Initial commit

--- src/Task2_7_4.java
+++ src/Task2_7_4.java
@@ -5,7 +5,7 @@ public class Task2_7_4 {
     public static void main(String[] args) {
         System.out.println("Задание : \n4.\tПосчитайте количество согласных букв в слове\n\nРешение: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите слово на криллице: ");
+        System.out.print("Введите слово на кириллице: ");
         String word = scanner.nextLine();
 
         String consonants = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩбвгджзйклмнпрстфхцчшщ";
