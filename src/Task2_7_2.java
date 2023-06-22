5a85be9dfc7f07522d3682637aa2e2ec3e8e74aa
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-22 12:37:29 +0300

    Initial commit

--- src/Task2_7_2.java
+++ src/Task2_7_2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Task2_7_2 {
 
     public static void main(String[] args) {
-        System.out.println("Задание : \n4.\tПосчитайте количество нулей в числе\n\nРешение: ");
+        System.out.println("Задание : \n2.\tПосчитайте количество нулей в числе\n\nРешение: ");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
         String num = scanner.nextLine();
